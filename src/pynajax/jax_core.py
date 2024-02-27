53b6f20d69e42f962887186bfe39264996259d0f
juhuntenburg <ju.huntenburg@gmail.com>
2024-02-27 16:17:05 +0100

    docstring

--- src/pynajax/jax_core.py
+++ src/pynajax/jax_core.py
@@ -10,6 +10,24 @@ _convolve_mat = jax.vmap(_convolve_vec, (None, 1), -1)
 
 
 def convolve(data, kernel):
+    """Convolves every dimension of data, except for the first which 
+    is assumed to be time, with every column of kernel.
+
+    Parameters
+    ----------
+    data : pynapple.Tsd, pynapple.TsdFrame, pynapple.TsdTensor
+        Pynapple timeseries object with data to be convolved
+    kernel : numpy.ndarray, jax.numpy.ndarray
+        1-D or 2-D array with kernel(s) to be used for convolution.
+        First dimension is assumed to be time.
+
+    Returns
+    -------
+    pynapple.Tsd, pynapple.TsdFrame, pynapple.TsdTensor
+        Pynapple timeseries object with convolved data. If kernel is a 1-D array,
+        the dimensions of the input data are retained. If kernel is a 2-D array,
+        another (last) dimension is added to store convolution with every column of kernels.
+    """
 
     # Store extra information of the pynapple object to add back later
     orig_shape = data.shape
@@ -24,12 +42,13 @@ def convolve(data, kernel):
 
     # Perform convolution
     if kernel.ndim == 0:
-        raise IOError("Provide a kernel with at least 1 dimension, current kernel has 0 dimensions")
+        raise IOError(
+            "Provide a kernel with at least 1 dimension, current kernel has 0 dimensions"
+        )
     elif kernel.ndim == 1:
         data = _convolve_vec(data, kernel).reshape(orig_shape)
     else:
         data = _convolve_mat(data, kernel).reshape((*orig_shape, kernel.shape[1]))
-
 
     # Recreate pynapple object
     data = np.asarray(data)
@@ -41,4 +60,3 @@ def convolve(data, kernel):
         data = nap.TsdTensor(t=time, d=data, time_support=time_support)
 
     return data
-
