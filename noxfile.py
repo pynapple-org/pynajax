71f5fd6480be3a2741939858b8b06eee2d133bfd
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-28 17:20:13 +0000

    resolve warns

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,13 @@ import nox
 @nox.session(name="tests", python=["3.10", "3.11", "3.12"])
 def tests(session):
     """Run the test suite."""
+    session.install("pytest")
     session.run("pytest")
 
 
 @nox.session(name="linters")
 def linters(session):
     """Run linters"""
+    session.install("ruff")
     session.run("ruff", "check", "src", "--ignore", "D")
 
