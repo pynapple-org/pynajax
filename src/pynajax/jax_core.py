3768dc6496281b780ba501623237ceb911e0def5
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-28 00:49:11 +0000

    linted docstrings

--- src/pynajax/jax_core.py
+++ src/pynajax/jax_core.py
@@ -1,3 +1,5 @@
+"""Vectorized one-dimensional convolution."""
+
 from functools import partial
 
 import jax
@@ -11,44 +13,112 @@ _convolve_mat = jax.vmap(_convolve_vec, (None, 1), -1)
 
 @jax.jit
 def _reshape_convolve_2d_kernel(tensor, kernel):
+    """
+    Reshape and convolve a N-Dimensional tensor with a 2D kernel.
+
+    Parameters
+    ----------
+    tensor : jax.numpy.ndarray
+        The input tensor to be convolved.
+    kernel : jax.numpy.ndarray
+        The convolution kernel.
+
+    Returns
+    -------
+    jax.numpy.ndarray:
+        The convolved tensor.
+    """
     shape_new = (*tensor.shape, kernel.shape[1])
     return _convolve_mat(tensor.reshape(tensor.shape[0], -1), kernel).reshape(shape_new)
 
 
 @jax.jit
 def _reshape_convolve_1d_kernel(tensor, kernel):
+    """
+    Reshape and convolve a N-Dimensional tensor with a 1D kernel.
+
+    Parameters
+    ----------
+    tensor : jax.numpy.ndarray
+        The input tensor to be convolved.
+    kernel : jax.numpy.ndarray
+        The convolution kernel.
+
+    Returns
+    -------
+    jax.numpy.ndarray:
+        The convolved tensor.
+    """
     return _convolve_vec(tensor.reshape(tensor.shape[0], -1), kernel).reshape(tensor.shape)
 
 
 @jax.jit
 def _jit_tree_convolve_2d_kernel(tree, kernel):
+    """
+    Convolve each epoch of a tree with a 2D kernel.
+
+    Parameters
+    ----------
+    tree : Any
+        Tree of tensors (usually list of arrays, one array per epoch) to be convolved.
+    kernel : jax.numpy.ndarray
+        The convolution kernel.
+
+    Returns
+    -------
+    jax.numpy.ndarray:
+        The concatenated convolved epochs.
+    """
     # Convolve each epoch
     func = partial(_reshape_convolve_2d_kernel, kernel=kernel)
     convolved_epochs = jax.tree_map(lambda x: func(x), tree)
+    # Concatenate leaves on the first axis and return
     return jnp.concatenate(jax.tree_leaves(convolved_epochs), axis=0)
+
 
 @jax.jit
 def _jit_tree_convolve_1d_kernel(tree, kernel):
+    """
+    Convolve each epoch of a tree with a 1D kernel.
+
+    Parameters
+    ----------
+    tree : Any
+        Tree of tensors (usually list of arrays, one array per epoch) to be convolved.
+    kernel : jax.numpy.ndarray
+        The convolution kernel.
+
+    Returns
+    -------
+    jax.numpy.ndarray:
+        The concatenated convolved epochs.
+    """
     # Convolve each epoch
     func = partial(_reshape_convolve_1d_kernel, kernel=kernel)
     convolved_epochs = jax.tree_map(lambda x: func(x), tree)
-    # Concatenate the convolved epochs
+    # Concatenate leaves on the first axis and return
     return jnp.concatenate(jax.tree_leaves(convolved_epochs), axis=0)
 
 
 def construct_nap(time, data, time_support, columns):
     """
+    Construct a pynapple timeseries object.
 
     Parameters
     ----------
-    time
-    data
-    time_support
-    columns
+    time : numpy.ndarray
+        Array of time values.
+    data : numpy.ndarray
+        Array of data values.
+    time_support : pynapple.IntervalSet
+        Index representing the time support.
+    columns : list or None
+        List of column names.
 
     Returns
     -------
-
+    : pynapple.Tsd, pynapple.TsdFrame, pynapple.TsdTensor
+        The constructed pynapple timeseries object.
     """
     if data.ndim == 1:
         data = nap.Tsd(t=time, d=data, time_support=time_support)
@@ -60,7 +130,7 @@ def construct_nap(time, data, time_support, columns):
 
 
 def convolve_epoch(data, kernel):
-    """Convolves a single continuous temporal epoch
+    """Convolve a single continuous temporal epoch.
 
     Parameters
     ----------
@@ -77,7 +147,6 @@ def convolve_epoch(data, kernel):
         the dimensions of the input data are retained. If kernel is a 2-D array,
         another (last) dimension is added to store convolution with every column of kernels.
     """
-
     # Store extra information of the pynapple object to add back later
     time = data.t
     time_support = data.time_support
@@ -101,8 +170,10 @@ def convolve_epoch(data, kernel):
 
 
 def convolve_intervals(data, kernel):
-    """Convolves every dimension of data, except for the first which
-    is assumed to be time, with every column of kernel.
+    """Convolve over the first dimension.
+
+    Convolve over the first dimension, vectorizing on every dimension of data,
+    with every column of kernel.
 
     Parameters
     ----------
@@ -119,7 +190,6 @@ def convolve_intervals(data, kernel):
         the dimensions of the input data are retained. If kernel is a 2-D array,
         another (last) dimension is added to store convolution with every column of kernels.
     """
-
     # Create a tree of pynapple timeseries objects for each epoch
     tree = [data.get(start, end).d for start, end in data.time_support.values]
 
@@ -137,6 +207,7 @@ def convolve_intervals(data, kernel):
 
 
 def convolve(data, kernel):
+    """One-dimensional convolution."""
     if len(data.time_support) > 1:
         return convolve_epoch(data, kernel)
     return convolve_intervals(data, kernel)
