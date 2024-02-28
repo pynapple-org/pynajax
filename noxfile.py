7d501b6b3a8cec88c47ee7f7912b0ef1da2bd642
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-28 16:36:38 +0000

    added linter

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox
+
+
+@nox.session(name="tests", python=["3.10", "3.11", "3.12"])
+def tests(session):
+    """Run the test suite."""
+    session.run("pytest")
+
+
+@nox.session(name="linters")
+def linters(session):
+    """Run linters"""
+    session.run("ruff", "check", "src", "--ignore", "D")
+
