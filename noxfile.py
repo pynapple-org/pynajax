a2cb9b9de77d2848d076512b114d6f59b0f77a4b
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-29 01:03:40 +0000

    removed install in nox

--- noxfile.py
+++ noxfile.py
@@ -4,13 +4,11 @@ import nox
 @nox.session(name="tests", python=["3.10", "3.11", "3.12"])
 def tests(session):
     """Run the test suite."""
-    session.install(".[dev]")
     session.run("pytest")
 
 
 @nox.session(name="linters")
 def linters(session):
     """Run linters"""
-    session.install("ruff")
     session.run("ruff", "check", "src", "--ignore", "D")
 
