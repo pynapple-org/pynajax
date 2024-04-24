0d9d87653f048a971cf61fea67bf75054adb7291
Guillaume Viejo <guillaume.viejo@gmail.com>
2024-04-24 12:41:15 -0400

    update with threshold

---
+++ src/pynajax/jax_core_threshold.py
@@ -0,0 +1,39 @@
+import jax
+import jax.numpy as jnp
+from .utils import _get_idxs, _get_slicing
+
+def threshold(time_array, data_array, starts, ends, thr, method):
+    n = time_array.shape[0]
+
+    idx_start, idx_end = _get_idxs(time_array, starts, ends)
+    idx_slicing = _get_slicing(idx_start, idx_end)
+
+    data_array = data_array[idx_slicing]
+    time_array = time_array[idx_slicing]
+
+    if method == "above":
+        ix = data_array > thr
+    elif method == "below":
+        ix = data_array < thr
+    elif method == "aboveequal":
+        ix = data_array >= thr
+    elif method == "belowequal":
+        ix = data_array <= thr
+
+    ix2 = jnp.diff(ix*1)
+
+    new_starts = time_array[1:][ix2==1]
+    new_ends = time_array[0:-1][ix2==-1]
+
+    if len(new_starts) < len(new_ends):
+        new_starts = jnp.hstack((jnp.array([time_array[0]]), new_starts))
+    elif len(new_starts) > len(new_ends):
+        new_ends = jnp.hstack((new_ends, jnp.array([time_array[-1]])))
+
+
+
+    return time_array[ix], data_array[ix], new_starts, new_ends
+
+
+    
+
