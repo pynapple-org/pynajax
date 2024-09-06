fd4338680de3f90ffdb7f9b42cfc539af4b29262
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-09-06 15:52:27 -0400

    fixed test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from pynajax.utils import _revert_epochs, _get_complement_slicing, _odd_ext_multiepoch
+from pynajax.utils import _revert_epochs, _get_complement_slicing, _odd_ext_multiepoch, _get_idxs
 import numpy as np
 from scipy.signal._arraytools import odd_ext
 import pynapple as nap
@@ -7,17 +7,17 @@ import pynapple as nap
 
 @pytest.mark.parametrize("time_array, starts, ends, expected_out", [
     (np.arange(10), [0, 4], [3, 10], np.array([3, 2, 1, 0, 9, 8, 7, 6, 5, 4])),  # Regular case
-    (np.arange(5), [0, 2.1], [2, 5], np.array([2, 1, 0, 4, 3])),                  # Smaller array
+    (np.arange(5), [0., 2.1], [2, 5], np.array([2, 1, 0, 4, 3])),                  # Smaller array
     (np.arange(6), [0], [6], np.array([5, 4, 3, 2, 1, 0])),                     # Single epoch
-    (np.arange(0), [], [], np.array([])),                                       # Empty array
-    (np.arange(10), [0, 3.1, 7.1], [3, 7, 10], np.array([3, 2, 1, 0, 7, 6, 5, 4, 9, 8])),  # Multiple epochs
-    (np.arange(10), [0, 3.1], [3, 3.2], np.array([3, 2, 1, 0])),                       # Zero-length epoch
+    (np.arange(10), [0., 3.1, 7.1], [3, 7, 10], np.array([3, 2, 1, 0, 7, 6, 5, 4, 9, 8])),  # Multiple epochs
+    (np.arange(10), [0., 3.1], [3., 3.2], np.array([3, 2, 1, 0])),                       # Zero-length epoch
     (np.arange(10), [2.1], [2.3], np.array([])),                                    # Start equals end
     (np.arange(10), [0], [10], np.array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])),       # Full array
-    (np.arange(10), [5, 7.1], [7, 7.5], np.array([7, 6, 5])),                          # Overlapping epochs
+    (np.arange(10), [5., 7.1], [7., 7.5], np.array([7, 6, 5])),                          # Overlapping epochs
 ])
 def test_revert_epoch(time_array, starts, ends, expected_out):
-    irev = _revert_epochs(time_array, np.array(starts), np.array(ends))
+    istart, iend = _get_idxs(time_array, starts, ends)
+    irev = _revert_epochs(istart, iend)
     assert np.array_equal(expected_out, time_array[irev]), f"Failed for starts={starts}, ends={ends}"
 
 
