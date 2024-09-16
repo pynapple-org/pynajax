83fff24234642c717391a9317a3c483e0c9312f5
Guillaume Viejo <guillaume.viejo@gmail.com>
2024-09-16 11:33:57 -0400

    UPdate

--- docs/examples/plot_benchmark_filtering.py
+++ docs/examples/plot_benchmark_filtering.py
@@ -8,6 +8,9 @@ import jax.numpy as jnp
 from time import perf_counter
 import matplotlib.pyplot as plt
 
+import warnings
+warnings.filterwarnings("ignore")
+
 # %%
 # Machine Configuration
 import jax
@@ -26,8 +29,6 @@ def get_mean_perf(tsd, mode, n=10):
 
 # %%
 
-
-
 def benchmark_time_points(mode):
     times = []
     for T in np.arange(1000, 100000, 20000):
@@ -41,20 +42,23 @@ def benchmark_time_points(mode):
 
 def benchmark_dimensions(mode):
     times = []
-    for n in np.arange(1, 100, 10):
-        time_array = np.arange(10000)/1000
+    T = 60000
+    for n in np.arange(1, 100, 20):
+        time_array = np.arange(T)/1000
         data_array = np.random.randn(len(time_array), n)
-        startend = np.linspace(0, time_array[-1], 10000//100).reshape(10000//200, 2)
+        startend = np.linspace(0, time_array[-1], T//100).reshape(T//200, 2)
         ep = nap.IntervalSet(start=startend[::2,0], end=startend[::2,1])
-        tsd = nap.TsdFrame(t=time_array, d=data_array, time_support=ep)
+        tsd = nap.TsdFrame(t=time_array, d=data_array)#, time_support=ep)
         times.append([n]+get_mean_perf(tsd, mode))
     return np.array(times)
 
 
 # %%
-# Increasing number of time points
+# # Increasing number of time points
 #
 # Calling with numba/scipy
+nap.nap_config.set_backend("numba")
+
 times_sinc_numba = benchmark_time_points(mode="sinc")
 times_butter_scipy = benchmark_time_points(mode="butter")
 
@@ -66,45 +70,81 @@ times_sinc_jax = benchmark_time_points(mode="sinc")
 times_butter_jax = benchmark_time_points(mode="butter")
 
 # # %%
-# # Figure
-# plt.figure(figsize = (16, 5))
-# plt.subplot(121)
-# for arr, label in zip(
-#     [times_sinc_numba, times_butter_scipy],
-#     ["Windowed-sinc", "Butter"],
-#     ):
-#     plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
-#     plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
-# plt.legend()
-# plt.xlabel("Number of time points")
-# plt.ylabel("Time (s)")
-# plt.title("Low pass filtering benchmark")
-# plt.subplot(122)
-# for arr, label in zip(
-#     [dims_sinc_numba, dims_butter_scipy],
-#     ["Windowed-sinc", "Butter"],
-#     ):
-#     plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
-#     plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
-# plt.legend()
-# plt.xlabel("Number of dimensions")
-# plt.ylabel("Time (s)")
-# plt.title("Low pass filtering benchmark")
-#
+# Figure
 
 
-# dims_sinc_numba = benchmark_dimensions(mode="sinc")
-# dims_butter_scipy = benchmark_dimensions(mode="butter")
+plt.figure(figsize = (16, 5))
+plt.subplot(121)
+for arr, label in zip(
+    [times_sinc_numba, times_sinc_jax],
+    ["Sinc (numba)", "Sinc (jax)"],
+    ):
+    plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
+    plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
+plt.legend()
+plt.xlabel("Number of time points")
+plt.ylabel("Time (s)")
+plt.title("Windowed-sinc low pass")
+
+plt.subplot(122)
+for arr, label in zip(
+    [times_butter_scipy, times_butter_jax],
+    ["Butter (scipy)", "Butter (jax)"],
+    ):
+    plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
+    plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
+
+plt.legend()
+plt.xlabel("Number of time points")
+plt.ylabel("Time (s)")
+plt.title("Butterworth filter low pass")
+# plt.show()
+
 
 # %%
+# # Increasing number of dimensions
 #
-# dims_sinc_jax = benchmark_dimensions(mode="sinc")
-# dims_butter_jax = benchmark_dimensions(mode="butter")
-#
-#
-#
+# Calling with numba/scipy
+nap.nap_config.set_backend("numba")
 
-#
+dims_sinc_numba = benchmark_dimensions(mode="sinc")
+dims_butter_scipy = benchmark_dimensions(mode="butter")
+
+# %%
+# Calling with jax
+nap.nap_config.set_backend("jax")
+
+dims_sinc_jax = benchmark_dimensions(mode="sinc")
+dims_butter_jax = benchmark_dimensions(mode="butter")
+
 # # %%
-# Saving
+# Figure
+
+
+plt.figure(figsize = (16, 5))
+plt.subplot(121)
+for arr, label in zip(
+    [dims_sinc_numba, dims_sinc_jax],
+    ["Sinc (numba)", "Sinc (jax)"],
+    ):
+    plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
+    plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
+plt.legend()
+plt.xlabel("Number of dimensions")
+plt.ylabel("Time (s)")
+plt.title("Windowed-sinc low pass")
+
+plt.subplot(122)
+for arr, label in zip(
+    [dims_butter_scipy, dims_butter_jax],
+    ["Butter (scipy)", "Butter (jax)"],
+    ):
+    plt.plot(arr[:, 0], arr[:, 1], "o-", label=label)
+    plt.fill_between(arr[:, 0], arr[:, 1] - arr[:, 2], arr[:, 1] + arr[:, 2], alpha=0.2)
+
+plt.legend()
+plt.xlabel("Number of dimensions")
+plt.ylabel("Time (s)")
+plt.title("Butterworth filter low pass")
+plt.show()
 
