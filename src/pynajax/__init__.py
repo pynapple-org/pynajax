a9d0779bd8c8edbcfb050c021e5e33914c63a5da
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-04-04 16:42:52 -0400

    fix

--- src/pynajax/__init__.py
+++ src/pynajax/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
 from importlib.metadata import version as _get_version
 
 try:
-    __version__ = _get_version("pynapple")
+    __version__ = _get_version("pynajax")
 except _PackageNotFoundError:
     # package is not installed
     pass
