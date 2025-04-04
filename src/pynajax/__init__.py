1661cafcf50880a437119da1c692115f2d2fabad
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-04-04 16:10:58 -0400

    Update ci

--- src/pynajax/__init__.py
+++ src/pynajax/__init__.py
@@ -1,3 +1,10 @@
-__version__ = "0.2.0"
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _get_version
+
+try:
+    __version__ = _get_version("pynapple")
+except _PackageNotFoundError:
+    # package is not installed
+    pass
 # from .jax_core_bin_average import bin_average
 # from .jax_core_convolve import convolve
